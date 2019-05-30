957ceccd1b295f365ec1b18093d8505ff2d3e93b
fredy p <fredy.pedraza.esteban@hotmail.com>
2019-05-30 11:31:10 -0500

    grafica con error

--- Capacitacion/Angular-Bootstrap-with-Material-Design/src/app/weather/weather.component.ts
+++ Capacitacion/Angular-Bootstrap-with-Material-Design/src/app/weather/weather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 
 
 @Component({
@@ -8,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit() {
     //Lista de ciudades
@@ -25,8 +28,8 @@ export class WeatherComponent implements OnInit {
   ];
 
   dat(city) {
-    const p = this.weatherList;
-    var w = this.dato_json(city);
+    const p = this.weatherList;    
+    var w = this.dato_json(city,'http://api.openweathermap.org/data/2.5/weather?q=',',cl,uk&APPID=a3803f4e13b97e469e7b590e8d24465f');
     w.onload = weather;
     function weather() {
       var h = w.response;
@@ -35,8 +38,7 @@ export class WeatherComponent implements OnInit {
       var address = h["wind"].deg;
       var icon = h["weather"][0].icon;
       icon = "http://openweathermap.org/img/w/" + icon + ".png"
-      p.push({ city: city, temp: com_temp(temp), speed: speed, address: address, icon: icon });
-
+      p.push({ city: city, temp: com_temp(temp), speed: speed, address: address, icon: icon });      
     };
     function com_temp(temp) {
       var resul = temp - 273.15;
@@ -44,9 +46,8 @@ export class WeatherComponent implements OnInit {
       return resul;
     }
   };
-  dato_json(city) {
-    var url2 = ',cl,uk&APPID=a3803f4e13b97e469e7b590e8d24465f'
-    var requestURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + url2;
+  dato_json(city, url, url2) {    
+    var requestURL = url + city + url2;
     var request = new XMLHttpRequest();
     request.open("GET", requestURL);
     request.responseType = 'json';
@@ -56,12 +57,12 @@ export class WeatherComponent implements OnInit {
   //Chart
   public chartType: string = 'line';
   public chartDatasets: Array<any> = [
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'My Second dataset' }
+    { data: [28, 48, 40], label: 'My Second dataset' },    
   ];
   public lista: Array<any> = [
-
+    
   ];
-  public chartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May'];
+  public chartLabels: Array<any> = [];
 
   public chartColors: Array<any> = [
     {
@@ -80,12 +81,28 @@ export class WeatherComponent implements OnInit {
   };
   public chartClicked(e: any): void { }
   public chartHovered(e: any): void { }
-  pronostico(dato) {
-    this.lista.push({ data: [28, 48, 40, 19, 86, 27, 90], label: dato })
-    this.chartDatasets = this.lista;
+  city(dat) {                
+    const dt= this.chartLabels;    
+    const temporal=this.lista;
+    var time=this.dato_json(dat,'http://api.openweathermap.org/data/2.5/forecast?q=',',cl,uk&APPID=a3803f4e13b97e469e7b590e8d24465f');   
+    time.onload=function(){      
+      var h=time.response;            
+      dt.push(h["list"][0].dt_txt);            
+      dt.push(h["list"][1].dt_txt);            
+      dt.push(h["list"][2].dt_txt);      
+      temporal.push({ data: [com_temp(h["list"][0].main.temp),com_temp(h["list"][1].main.temp),com_temp(h["list"][2].main.temp)], label: dat });      
+  }  
+    function com_temp(temp) {
+      var resul = temp - 273.15;
+      resul = Math.round(resul);
+      return resul;
+    }    
+    //Error 
+    console.log(temporal[0].label);    
+    //Error
+    this.chartDatasets=this.lista;        
   }
-  city(dat) {
-    alert(dat);
-  }
-  
+  deleteList(){           
+    console.log(this.chartDatasets[this.chartDatasets.length-1].label)    
+  }  
 }
