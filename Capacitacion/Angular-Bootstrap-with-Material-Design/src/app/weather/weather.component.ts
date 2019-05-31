79c4437b93a1eb8b0e47056ac25a6069273be38b
fredy p <fredy.pedraza.esteban@hotmail.com>
2019-05-31 09:26:47 -0500

    temperatura proximas horas 1

--- Capacitacion/Angular-Bootstrap-with-Material-Design/src/app/weather/weather.component.ts
+++ Capacitacion/Angular-Bootstrap-with-Material-Design/src/app/weather/weather.component.ts
@@ -10,8 +10,8 @@ import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 export class WeatherComponent implements OnInit {
 
   constructor() {
-    
-   }
+
+  }
 
   ngOnInit() {
     //Lista de ciudades
@@ -28,8 +28,8 @@ export class WeatherComponent implements OnInit {
   ];
 
   dat(city) {
-    const p = this.weatherList;    
-    var w = this.dato_json(city,'http://api.openweathermap.org/data/2.5/weather?q=',',cl,uk&APPID=a3803f4e13b97e469e7b590e8d24465f');
+    const p = this.weatherList;
+    var w = this.dato_json(city, 'http://api.openweathermap.org/data/2.5/weather?q=', ',cl,uk&APPID=a3803f4e13b97e469e7b590e8d24465f');
     w.onload = weather;
     function weather() {
       var h = w.response;
@@ -38,7 +38,7 @@ export class WeatherComponent implements OnInit {
       var address = h["wind"].deg;
       var icon = h["weather"][0].icon;
       icon = "http://openweathermap.org/img/w/" + icon + ".png"
-      p.push({ city: city, temp: com_temp(temp), speed: speed, address: address, icon: icon });      
+      p.push({ city: city, temp: com_temp(temp), speed: speed, address: address, icon: icon });
     };
     function com_temp(temp) {
       var resul = temp - 273.15;
@@ -46,7 +46,7 @@ export class WeatherComponent implements OnInit {
       return resul;
     }
   };
-  dato_json(city, url, url2) {    
+  dato_json(city, url, url2) {
     var requestURL = url + city + url2;
     var request = new XMLHttpRequest();
     request.open("GET", requestURL);
@@ -57,10 +57,11 @@ export class WeatherComponent implements OnInit {
   //Chart
   public chartType: string = 'line';
   public chartDatasets: Array<any> = [
-    { data: [28, 48, 40], label: 'My Second dataset' },    
+    { data: [28, 48, 40], label: 'My Second dataset' },
+
   ];
   public lista: Array<any> = [
-    
+
   ];
   public chartLabels: Array<any> = [];
 
@@ -81,28 +82,41 @@ export class WeatherComponent implements OnInit {
   };
   public chartClicked(e: any): void { }
   public chartHovered(e: any): void { }
-  city(dat) {                
-    const dt= this.chartLabels;    
-    const temporal=this.lista;
-    var time=this.dato_json(dat,'http://api.openweathermap.org/data/2.5/forecast?q=',',cl,uk&APPID=a3803f4e13b97e469e7b590e8d24465f');   
-    time.onload=function(){      
-      var h=time.response;            
-      dt.push(h["list"][0].dt_txt);            
-      dt.push(h["list"][1].dt_txt);            
-      dt.push(h["list"][2].dt_txt);      
-      temporal.push({ data: [com_temp(h["list"][0].main.temp),com_temp(h["list"][1].main.temp),com_temp(h["list"][2].main.temp)], label: dat });      
-  }  
+  city_temp(dat) {
+    
+    
+    const dt = this.chartLabels;
+    const temporal = this.lista;
+    var time = this.dato_json(dat, 'http://api.openweathermap.org/data/2.5/forecast?q=', ',cl,uk&APPID=a3803f4e13b97e469e7b590e8d24465f');
+    time.onload = function () {
+      var h = time.response;
+      for (var i = 0; i = dt.length; i++) {
+        dt.pop();
+      }
+      dt.push(h["list"][0].dt_txt);
+      dt.push(h["list"][1].dt_txt);
+      dt.push(h["list"][2].dt_txt);
+      temporal.push({ data: [com_temp(h["list"][0].main.temp), com_temp(h["list"][1].main.temp), com_temp(h["list"][2].main.temp)], label: "Temperatura: " + dat });
+    }
     function com_temp(temp) {
       var resul = temp - 273.15;
       resul = Math.round(resul);
       return resul;
-    }    
+    }
     //Error 
-    console.log(temporal[0].label);    
+    console.log(temporal[0].label);
     //Error
-    this.chartDatasets=this.lista;        
+    this.chartDatasets = this.lista;
   }
-  deleteList(){           
-    console.log(this.chartDatasets[this.chartDatasets.length-1].label)    
-  }  
+  deleteList() {
+    console.log(this.chartDatasets.length)
+    while (this.chartDatasets[1] != null) {
+      this.chartDatasets.pop();
+    }    
+    this.lista = this.chartDatasets;    
+    if(this.lista[0]!=null){
+      this.lista
+    }
+    console.log(this.lista.length + " " + this.chartDatasets.length)
+  }
 }
