300328bc5d2fa2d62d6eb35341cf6146895bdaf6
fredy p <fredy.pedraza.esteban@hotmail.com>
2019-06-17 09:12:54 -0500

    Grafica P

--- Capacitacion/Angular-Bootstrap-with-Material-Design/src/app/weather/weather-table/weather-table.component.ts
+++ Capacitacion/Angular-Bootstrap-with-Material-Design/src/app/weather/weather-table/weather-table.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {WeatherServise}from '../../service/weather.service'
-import { modalConfigDefaults } from 'angular-bootstrap-md/lib/modals/modal.options';
+import { WeatherServise } from '../../service/weather.service'
+
 
 @Component({
   selector: 'app-weather-table',
@@ -9,21 +9,18 @@ import { modalConfigDefaults } from 'angular-bootstrap-md/lib/modals/modal.optio
 })
 export class WeatherTableComponent implements OnInit {
 
- @Input() weatherList: Array<any> = [];  
- constructor(private weatherS:WeatherServise) {     
-}
+  @Input() weatherList: Array<any> = [];
+  constructor(private weatherS: WeatherServise) {
+  }
 
 
-  ngOnInit() {    
-    
+  ngOnInit() {
+
   };
   //Modal
-  ver(){
-    console.log(this.weatherS.getDat())
-  }
   public chartType: string = 'line';
   public chartDatasets: Array<any> = [
-    { data: [28, 48, 40], label: 'My Second dataset' },
+    { data: [28, 48, 40], label: 'Grafica Predeterminada'},
 
   ];
   public lista: Array<any> = [
@@ -46,40 +43,29 @@ export class WeatherTableComponent implements OnInit {
   public chartOptions: any = {
     responsive: true
   };
-  
+
   public chartClicked(e: any): void { };
   public chartHovered(e: any): void { };
-  city_temp(city) {    
+  city_temp(city) {
     const URL1 = 'http://api.openweathermap.org/data/2.5/forecast?q=';
-    const URL2 = ',cl,uk&APPID=a3803f4e13b97e469e7b590e8d24465f';    
+    const URL2 = ',cl,uk&APPID=a3803f4e13b97e469e7b590e8d24465f';
     const dt = this.chartLabels;
     const temporal = this.lista;
     let time = this.weatherS.dato_json(URL1, city, URL2);
-    time.onload =  () => {
+    time.onload = () => {
       let h = time.response;
       for (var i = 0; i = dt.length; i++) {
         dt.pop();
-      }      
+      }
       dt.push(h["list"][0].dt_txt);
       dt.push(h["list"][1].dt_txt);
       dt.push(h["list"][2].dt_txt);
       temporal.push({ data: [this.weatherS.com_temp(h["list"][0].main.temp), this.weatherS.com_temp(h["list"][1].main.temp), this.weatherS.com_temp(h["list"][2].main.temp)], label: "Temperatura: " + city });
-    }    
+    }
     //Error 
     console.log(temporal[0].label);
     //Error
     this.chartDatasets = this.lista;
   }
-  deleteList() {
-    console.log(this.chartDatasets.length);
-    while (this.chartDatasets[1] != null) {
-      this.chartDatasets.pop();
-    }
-    this.lista = this.chartDatasets;
-    if (this.lista[0] != null) {
-      this.lista;
-    }
-    console.log(this.lista.length + " " + this.chartDatasets.length);
-  }
-  
+
 }
